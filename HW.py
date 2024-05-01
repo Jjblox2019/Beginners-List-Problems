e80cc7daa2d8b5d5d960442892b39d69383d5a0a
Jjblox2019 <126438138+Jjblox2019@users.noreply.github.com>
2024-05-01 16:28:45 +1200

    Create HW.py

---
+++ HW.py
@@ -0,0 +1,9 @@
+STARTER
+numsList = [7, 6, 23, 8.18, 18, 8, 7.2, 85, 915, 12]
+print(max(numsList))
+print(min(numsList))
+print(sum(numsList) / len(numsList))
+SAME START AND END
+I Like Pesto (a lot)
+iLikePest = []
+otherFoods = []
